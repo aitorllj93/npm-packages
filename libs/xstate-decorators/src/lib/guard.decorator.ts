ef3a126cf60402aaa384bc7584f5a5e95489ead6
Aitor <aitorllj93@gmail.com>
2021-09-03 17:19:32 +0200

    fix(xstate-decorators): update guard decorator typings (#12)

--- libs/xstate-decorators/src/lib/guard.decorator.ts
+++ libs/xstate-decorators/src/lib/guard.decorator.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-types */
 
 import { DecoratorStateMachineExecutor } from './decorator-state-machine.executor';
-import { StateMachineExecutor } from './state-machine.executor';
 
 export const Guard = (name: string) => {
-  return function (target: StateMachineExecutor, propertyKey: string | symbol) {
-    const decoratorTargetPrototype = target as unknown as DecoratorStateMachineExecutor;
+  return function (target: unknown, propertyKey: string | symbol) {
+    const decoratorTargetPrototype = target as DecoratorStateMachineExecutor;
     decoratorTargetPrototype.options = decoratorTargetPrototype.options || {};
     decoratorTargetPrototype.options.guards = decoratorTargetPrototype.options.guards || {};
     Object.assign(decoratorTargetPrototype.options.guards, {
